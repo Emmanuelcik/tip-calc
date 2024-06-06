ff5cfa818c6f94f5ac0928c1ef07997f107a90a8
Emmmanuel Lopez <jesusemmcik@gmail.com>
2024-06-05 23:59:48 -0600

    handle duplicated items in useOrder hook

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -4,8 +4,19 @@ export default function useOrder() {
   const [order, setOrder] = useState<OrderItem[]>([]);
 
   const addItem = (item: MenuItem) => {
-    const newItem: OrderItem = { ...item, quantity: 1 };
-    setOrder([...order, newItem]);
+    const itemExists = order.find((orderItem) => orderItem.id === item.id);
+    if (itemExists) {
+      const updatedOrder = order.map((orderItem) =>
+        orderItem.id === item.id
+          ? { ...orderItem, quantity: orderItem.quantity + 1 }
+          : orderItem
+      );
+      setOrder(updatedOrder);
+    } else {
+      const newItem: OrderItem = { ...item, quantity: 1 };
+      setOrder([...order, newItem]);
+    }
+    console.log(order);
   };
   return { addItem };
 }
